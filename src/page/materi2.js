96104807cb1cf3555f46a36d441518b723e45a6f
Nestiawan Ferdiyanto <nestiawan.118140190@student.itera.ac.id>
2021-04-23 17:05:12 +0700

    materi 2

--- src/page/materi2.js
+++ src/page/materi2.js
@@ -1,16 +1,54 @@
-import React, { useState } from 'react'
+/* eslint-disable eqeqeq */
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable no-useless-constructor */
+import React, { Component } from 'react'
 
-function Example() {
-    const [count, setCount] = useState(0);
+class materi2 extends Component {
+    state = {
+        isLoading: true,
+        data: []
+    }
 
-    return (
-        <div>
-            <p>You clicked {count} times</p>
-            <button onClick={() => setCount(count + 1)}>
-                Click me
-            </button>
-        </div>
-    );
+    componentWillMount = () => {
+        console.log("ComponentDidmount Sedang Berjalan")
+        fetch("https://pwlapilumen.herokuapp.com/webScraping") 
+        .then(res => {
+            if( res.status === 200 )
+                return res.json()
+            else
+                console.log("error");
+        }).then( resJson => {
+            console.log("Mengatur State.data")
+            this.setState({
+                data: resJson,
+                isLoading: false,
+            })
+            console.log("Success")
+        })
+    };
+
+    render() {
+
+        return (
+            <div className="d-flex">
+                { 
+                    
+                    (this.state.isLoading == false) ? 
+                        this.state.data.map((datasAPI, index) => 
+                            <div className="card" key={index}>
+                                <a href="">
+                                    <img src={datasAPI.images} width="100%" height="190"/>
+                                    <h3> {datasAPI.title} </h3> 
+                                    <p> {datasAPI.date} </p> 
+                                </a>
+                            </div>
+                        ) 
+                    : <div className="LoadingData"> Tunggu Sebentar... </div>
+                }
+            </div>
+        )
+    }
 }
 
-export default Example;
+export default materi2;
